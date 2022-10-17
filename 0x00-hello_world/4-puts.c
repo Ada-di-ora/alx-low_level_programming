5d2075beff7647bb11da3438264fc7c61ed2d7e6
Ada-di-ora <ebubemi@gmai.com>
2022-10-17 14:28:42 -0700

    okay

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /**
- *  main - prints the "line" varible to standard output
- *  Return: 0 is return if succesful
+ *   main - prints the "line" varible to standard output
+ *   Return: 0 is return if succesful
  */
 int main(void)
 {
-	char line[] = "\"Programming is like building a multilingual puzzle";
-	puts(line);
-
-	return (0);
+char line[] = "\"Programming is like building a multilingual puzzle";
+puts(line);
+return (0);
 }
+
