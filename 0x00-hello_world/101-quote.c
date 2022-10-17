5ccc5f23a3dcc114fc229fb8dac84d807927b105
Ada-di-ora <ebubemi@gmai.com>
2022-10-17 13:59:36 -0700

    hard

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - this is a program that prints the belo line of text to standard error
+ *   Return: returns 1 if successful
+ */
+int main(void)
+{
+	const char quote[] = "and that piece of art is useful";
+	fprintf(stderr, "%s\" - Dora Korpar, 2015-10-19\n", quote);
+	return (1);
+}
+
