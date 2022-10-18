2ea80f5fc2f86fde0da48f09e0c1cc4df689bf05
Ada-di-ora <ebubemi@gmai.com>
2022-10-18 12:32:17 -0700

    nine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - this is the time to live
+ *  yes it is my brother halleluyah
+ *  Return: come back home my boy
+*/
+int main(void)
+{
+int d = '0';
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
