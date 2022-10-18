2cdd8a8f111f1028504c77f3dfd063fa0d3c0219
Ada-di-ora <ebubemi@gmai.com>
2022-10-18 11:50:59 -0700

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *   main - who am i if not seen through your eyes
+ *   this is just a lyric from a song i jamming
+ *   Return: 0 if successful
+*/
+int main(void)
+{
+int a = '0';
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
