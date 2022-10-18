b6c7e503eb0746bd1a5880649a75ce20c0bb818a
Ada-di-ora <ebubemi@gmai.com>
2022-10-18 12:38:31 -0700

    zreo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,14 +6,11 @@
  * main - prints if the number is positive, negative or 0
  * Return: Always 0
 */
-
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)	
 {
 printf("%d is positive\n", n);
@@ -28,4 +25,3 @@ printf("%d is negative\n", n);
 }
 return (0);
 }
- 
