494622c1ae1affee5f7708be260dde4f5ac2b68a
Ada-di-ora <ebubemi@gmai.com>
2022-10-18 12:26:14 -0700

    eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *    main - this na code
+ *    no even whine me
+ *    Return: returns 0 idf successful
+ */
+int main(void)
+{
+int C = '0';
+char d;
+for (C = '0'; C <= '9'; C++)
+{
+putchar(C);
+}
+for (d = 'a'; d <= 'f'; d++)
+{
+putchar(d);
+}
+putchar('\n');
+return (0);
+}
