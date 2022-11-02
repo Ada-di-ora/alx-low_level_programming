7e89d2105a4446dc42531239437e994fd7f16d43
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-02 15:29:48 -0700

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,2 +1,20 @@
-can i have your number
+#include "main.h"
+#include <stdio.h>
 
+/**
+ **_strpbrk - searches a string for any set of bytes
+ *@s: the string to be parsed
+ *@accept: the characters to look for
+ *Return: (s) a pointer to the first matching instance, or 0 if no matches
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	for (; *s != '\0'; s++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (*s == accept[j])
+				return (s);
+	return (NULL);
+}
