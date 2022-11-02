2e72cb763efb3c0e560530d8db9e650c18694d8d
Ada-di-ora <ebubemi@gmai.com>
2022-11-02 15:24:23 -0700

    three

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *  _strspn - gets the length of a prefix substring
+ *  @s: the string to check
+ *  @accept: the characters (bytes) that can be accepted
+ *  Return: (count) the length of the initial matching see
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, gonext;
+unsigned int count = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+gonext = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+gonext = 1;
+count++
+}
+}
+if (gonext == 0)
+return (count);
+}
+return (0);
+}
+
