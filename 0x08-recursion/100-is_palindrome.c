e79d7c84609690def57b08f78b01038fd8f309db
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-02 16:58:37 -0700

    Update 100-is_palindrome.c
    hundred

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,27 +1,47 @@
 #include "main.h"
 
-/**
- *wildcmp - checks if two strings are identical
- *@s1: the first string to check
- *@s2: the second string to check
- *Return: 1 if the strings are identical, 0 otherwise
- */
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
-int wildcmp(char *s1, char *s2)
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
 {
-	if (*s2 == '*')
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1));
-		else if (*(s2 + 1) != *s1 && *s1 != '\0')
-			return (wildcmp(s1 + 1, s2));
-		else
-			return (wildcmp(s1, s2 + 1));
-	else if (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-			return (1);
-		else
-			return (wildcmp(s1 + 1, s2 + 1));
-	}
-	return (0);
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
