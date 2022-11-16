f26ba6411081c1f8a61805c1af60e0d3945f9760
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-16 11:36:38 -0800

    Update 1-args.c
    one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - prints the number of arguments passed into it
+ * @argc: the number of arguments passed in
+ * @argv: an array of the arguments passed in
+ * Return: Always 0.
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc - 1);
+	printf("%i\n", argc - 1);
 	return (0);
 }
