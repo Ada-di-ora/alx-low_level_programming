80829584011e1cce030d21602a3aacac12faeee2
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-16 14:34:39 -0800

    Update 3-alloc_grid.c
    three return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,10 +2,9 @@
 #include "main.h"
 
 /**
- * **alloc_grid - creates a two dimensional array of ints
+ * alloc_grid - creates a two dimensional array of ints
  * @width: width of the matrix
  * @height: height of the matrix
- *
  * Return: pointer to the created matrix (Success)
  * or NULL (Error)
  */
