5f7fb969c424170959923edcc1a205baabdc514d
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-16 12:18:33 -0800

    Update 0-create_array.c
    zero return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,26 @@
-nothing
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of characters, and
+ * initializes it with a specific char
+ * @size: the size of the array
+ * @c: the char to fill the array with
+ * Return: a pointer to the created array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL || size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
