e2576ca73d0c9b63c189cc0cadb22c1ed9df6c41
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-20 13:15:00 -0800

    Update 5-free_dog.c
    five

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,18 @@
-good
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees an input dog_t structure
+ * @d: the input structure
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
