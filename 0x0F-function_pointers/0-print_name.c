0115f2b0f1c2d83365ce540ef3ef3380694ecaa9
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-21 12:40:24 -0800

    Update 0-print_name.c
    function name pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
-good
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to be printed
+ * @f: pointer to the print_name function
+ * Return: nothing
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
