54ce188e621ebd57a326ccea203ce30b7fbcd003
Ada-di-ora <113859668+Ada-di-ora@users.noreply.github.com>
2022-11-29 07:36:25 -0800

    Update 1-listint_len.c
    returns elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,25 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: the input listint_t list
  *
- * Return: number of nodes
+ * Return: the number of nodes.
  */
+
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	unsigned int i;
 
-	while (h)
+	for (i = 0; h; i++)
 	{
-		num++;
+		/*
+		 * if (!h->n)
+		 * printf("%d\n", h->n, "(nil)");
+		 * else
+		 * printf("%d\n", h->n);
+		 */
 		h = h->next;
 	}
-
-	return (num);
+	return (i);
 }
